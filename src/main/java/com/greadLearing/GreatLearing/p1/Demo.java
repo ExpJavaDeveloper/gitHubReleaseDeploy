beb79826efc8e7c85dc59cbb5b42878d588d5bb5
ExpJavaDeveloper <ietverma143@gmail.com>
2024-10-13 13:27:38 +0530

    Update Demo.java

--- src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
+++ src/main/java/com/greadLearing/GreatLearing/p1/Demo.java
@@ -3,7 +3,7 @@ package com.greadLearing.GreatLearing.p1;
 public class Demo {
 
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass-update class");
         // this is the demo class
     }
 }
